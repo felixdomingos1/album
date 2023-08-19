1cb3b38b82bcfe6920687c249d13bbb7c92e5d10
Paulino Passil <passilpaulino@gmail.com>
2023-08-19 20:28:14 +0100

    feat: adiciona arquivo que inicializa app

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "express-async-errors";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./docs/swagger.json";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+export { app };
