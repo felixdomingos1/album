2532f0f70a4a86d11baab69561a46feebb856944
Paulino Passil <passilpaulino@gmail.com>
2023-08-20 11:58:10 +0100

    feat: adiciona rotas na app

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,15 @@ import cors from "cors";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./docs/swagger.json";
+import { routes } from "./routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(routes);
+app.use(errorHandler);
 
 export { app };
