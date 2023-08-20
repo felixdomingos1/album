fc80f8673c3e00bf6e51a3ab857a9a63b7cd8509
Paulino Passil <passilpaulino@gmail.com>
2023-08-20 11:55:51 +0100

    feat: adiciona rotas de artista

---
+++ src/routes/artiste.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createArtiste } from "../modules/artiste/useCases/createArtiste";
+import multer from "multer";
+import { multerConfig } from "../config/multer";
+
+const artisteRouter = Router();
+
+artisteRouter.post("/", multer(multerConfig).single("image"), (req, res) => {
+  return createArtiste.handle(req, res);
+});
+
+export { artisteRouter };
