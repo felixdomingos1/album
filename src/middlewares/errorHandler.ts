8cbe445c27746f44edabaae133148a6d62f8d00f
Félix Domingos <97100392+felixdomingos1@users.noreply.github.com>
2023-12-25 00:36:17 +0100

    Update errorHandler.ts
    Uma vez utilizado ErrorRequestHandler do Express para a função errorHandler.
    Simplificou a lógica desestruturando o status e a mensagem do objeto err.
    Empreguei um operador ternário para determinar a resposta com base no fato de err ser uma instância de AppError.
    Consolidou a lógica de resposta em uma estrutura mais concisa.

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,23 +1,13 @@
-import { NextFunction, Response, Request } from "express";
+import { NextFunction, Response, Request, ErrorRequestHandler } from "express";
 import { AppError } from "../errors/AppError";
 
-function errorHandler(
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  if (err instanceof AppError) {
-    return res.status(err.status).json({
-      error: err.message,
-      status: err.status,
-    });
-  }
-
-  return res.status(500).json({
-    message: err.message,
-    status: 500,
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const { status = 500, message } = err instanceof AppError ? err : { message: err.message };
+  
+  res.status(status).json({
+    error: message,
+    status,
   });
-}
+};
 
 export { errorHandler };
