c6e4d5b171e76b534370f23a75a2954395bb22bf
Félix Domingos <97100392+felixdomingos1@users.noreply.github.com>
2023-12-25 00:13:29 +0100

    Update multer.ts

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -2,35 +2,32 @@ import multer from "multer";
 import path from "path";
 import { v4 as uuidV4 } from "uuid";
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+
+const storage = multer.diskStorage({
+  destination: TMP_FOLDER,
+  filename: (req, file, cb) => {
+    const uuid = uuidV4();
+    const fileExtension = file.originalname.split(".").pop();
+    const fileName = `${uuid}.${fileExtension}`;
+    cb(null, fileName);
+  },
+});
 
 const multerConfig = {
-  directory: tmpFolder,
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (req, file, cb) => {
-      const uuid = uuidV4();
-
-      const fileName = `${uuid}.${file.originalname.split(".").pop()}`;
-
-      return cb(null, fileName);
-    },
-  }),
+  directory: TMP_FOLDER,
+  storage,
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req: any, file: any, cb: any) => {
-    const allowedMimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "application/pdf",
-    ];
+    const allowedMimes = ["image/jpeg", "image/pjpeg", "image/png", "application/pdf"];
+    const isValidMimeType = allowedMimes.includes(file.mimetype);
 
-    if (allowedMimes.includes(file.mimetype)) {
+    if (isValidMimeType) {
       cb(null, true);
     } else {
-      cb(new Error("Extensão de imagem inválida!"));
+      cb(new Error("Invalid image extension!"));
     }
   },
 };
